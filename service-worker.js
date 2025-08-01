dbb646d290c1861bb1b289565a9f04fe553f176d
FenixASdev <a.d.souza.ads35@gmail.com>
2025-07-31 22:25:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,10 +16,30 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting(); // força ativação imediata
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() =>
+        caches.match('/offline.html') // Se desejar, adicione esse fallback
+      )
+    )
   );
 });
